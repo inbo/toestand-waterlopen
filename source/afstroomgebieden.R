659c3af1d8cac1868f5e9535fd5bff8ee417565b
DE LOMBAERDE <emiel.delombaerde@inbo.be>
2025-08-14 12:17:30 +0200

    script voor afstroomgebieden, landgebruik en overstorten gesplitst

--- source/afstroomgebieden.R
+++ source/afstroomgebieden.R
@@ -124,15 +124,15 @@ watersheds_nested <- watersheds_nested %>%
       mutate(rowname = as.integer(rowname))
   )
 watersheds_nested <- watersheds_nested %>%
-  mutate(oppervlakte = st_area(geom))
+  mutate(oppervlakte = st_area(geometry))
 
 if (!file.exists(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_nested_all.gpkg"))) {
 st_write(watersheds_nested, here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_nested_all.gpkg"), delete_dsn = T)
 }
 
-watersheds_nested <- st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_nested_all.gpkg"))
+watersheds_nested <- st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_nested_all.gpkg")) #gpkg met alle afstroomgebieden!
 
-# buffered watersheds
+# buffered watersheds; sommige afstroomgebieden zijn zo groot al een bekken. Hier overleg ik ze met een cirkelvormige buffer van 5km om dit in te perken.
 
 locations_buffer <- locations %>%
   mutate(buffer = st_buffer(geometry, dist = 5000))
@@ -169,13 +169,13 @@ result <- buffers %>%
 # Optional: drop empty geometries (e.g., no intersection)
 watersheds_buffered <- result %>% filter(!st_is_empty(geometry))
 watersheds_buffered <- watersheds_buffered %>%
-  mutate(oppervlakte = st_area(geom))
+  mutate(oppervlakte = st_area(geometry))
 
 if (!file.exists(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg"))) {
 st_write(watersheds_buffered, here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg"), delete_dsn = T)
 }
 
-st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg"))
+st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg")) # gpkg met afstroomgebieden ingeperkt tot 5km buffer.
 
 # met Watershed tool ----
 # # dit is minder realistische om dat hier geen geneste watersheds uit voortkomen bij punten die sequentieel in een waterloop liggen
@@ -221,7 +221,6 @@ my_palette <- cm.colors(9)
 locs <- locations %>%
   filter(meetplaats == random_meetplaats)
 
-
 mapview(locs, zcol = "meetplaats") +
   mapview(ws, zcol = "meetplaats", alpha.regions = 0) +
   mapview() +
@@ -263,206 +262,3 @@ mapview(vha_bekkens, alpha.regions = 0, legend = FALSE) +
   mapview(watersheds_nested, zcol = "meetplaats", legend = FALSE) +
   mapview(streams_sf) +
   mapview(wlas, color = "red")
-
-# landgebruik binnen afstroomgebied
-
-landuse_raster <- raster::raster(here("data", "ruw", "landgebruik", "niveau1_vla_2022_v3.tif"))
-watersheds_buffered <- st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg"))
-watersheds_buffered <- st_transform(watersheds_buffered, crs = crs(landuse_raster))
-plot(landuse_raster)
-
-# Define input and output paths
-output_table <- here("data", "verwerkt", "landgebruik", "zonal_histogram_landgebruik_afstroomgebieden.gpkg")
-
-# Run the zonal histogram algorithm
-
-landuse_raster[landuse_raster < 1 | landuse_raster > 25] <- NA # om vreemde grote waarde als outputkolom te voorkomen
-if (!file.exists(output_table)) {
-
-qgis_run_algorithm("native:zonalhistogram",
-                   INPUT_RASTER = landuse_raster,
-                   INPUT_VECTOR = watersheds_buffered,
-                   RASTER_BAND = 1,
-                   COLUMN_PREFIX = "VALUE_",  # Prefix for land-use classes
-                   OUTPUT = output_table)
-}
-
-watershed_landuse0 <- st_read(output_table)
-landuse_oever0 <- read_excel(path = here("data", "ruw", "landgebruik", "mi_meetpunten_lu_buffer.xlsx"))
-landuse_oever0[["VALUE_13"]] <- 0
-landuse_buffer0 <- read_excel(path = here("data", "ruw", "landgebruik", "mi_meetpunten_lu_cirk_min50m.xlsx"))
-
-
-convert_pixels_to_percentages <- function(data) {
-  data <- data %>%
-    mutate(
-      total_pixels = rowSums(across(starts_with("VALUE_")), na.rm = TRUE),
-      across(starts_with("VALUE_"), ~ .x / total_pixels * 100, .names = "{.col}_pct")
-    )
-
-  return(data)
-}
-
-watershed_landuse <- convert_pixels_to_percentages(data = watershed_landuse0)
-
-landuse_oever <- convert_pixels_to_percentages(data = landuse_oever0)
-landuse_buffer <- convert_pixels_to_percentages(data = landuse_buffer0)
-
-st_write(watershed_landuse, here("data", "verwerkt", "landgebruik","finale_watershed_landgebruik.gpkg"), delete_dsn = TRUE)
-save(landuse_oever, file = here("data", "verwerkt", "landgebruik","landgebruik_oever.Rdata"))
-save(landuse_buffer, file = here("data", "verwerkt", "landgebruik","landgebruik_buffer.Rdata"))
-
-# aantal overstorten in een afstroomgebied ----
-
-overstorten_basis_locaties <- st_read(here("data", "ruw", "overstorten", "P_OS_basis.shp"))
-overstorten_uitlaat_vha <- st_read(here("data", "ruw", "overstorten", "P_OS_uitlaat_VHA.shp"))
-
-watersheds_buffered <- st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg"))
-
-# Perform spatial join: Assign points to watersheds
-points_in_watersheds <- st_join(overstorten_uitlaat_vha, watersheds_buffered, left = FALSE)
-
-# Count points per watershed
-watershed_point_counts <- points_in_watersheds %>%
-  count(meetplaats) %>%
-  rename(aantal_overstorten = n)
-
-# Merge counts back into watersheds
-watersheds_aantal_overstorten <- watersheds_buffered %>%
-  left_join(watershed_point_counts %>%
-              st_drop_geometry(),
-            by = "meetplaats") %>%
-  mutate(aantal_overstorten = replace_na(aantal_overstorten, 0)) %>%
-  st_drop_geometry()
-
-
-# Save the output
-save(watersheds_aantal_overstorten, file = here("data", "verwerkt", "overstorten", "mi_meetpunten_aantal_overstorten_afstroomgebied.rdata"))
-
-# vuilvracht van overstorten in een afstroomgebied met buffer en afstandsgewogen ----
-
-vuilvracht_overstorten <- st_read(here("data", "ruw" , "overstorten", "vuilvracht", "OS_maart2025.shp"))
-
-
-# Perform spatial join: Assign points to watersheds
-points_in_watersheds <- st_join(vuilvracht_overstorten, watersheds_buffered, left = FALSE)
-
-points_in_watersheds
-
-locations_cum_gewogen_vuilvracht <- locations
-
-
-# --- 2. Functie voor afstandsgewogen BZV-berekening ---
-
-# Deze functie berekent de gewogen BZV voor EEN ENKEL meetpunt
-calculate_weighted_bzv <- function(meetplaats_id_val, overstorten_sf, meetplaatsen_sf, exponent = 1) {
-  # Haal de specifieke meetplaats geometrie op
-  current_location <- meetplaatsen_sf %>%
-    filter(meetplaats == meetplaats_id_val)
-
-  # Haal de overstorten op die bij dit meetpunt horen
-  relevant_overstorten <- overstorten_sf %>%
-    filter(meetplaats == meetplaats_id_val)
-
-  # Als er geen relevante overstorten zijn, retourneer 0
-  if (nrow(relevant_overstorten) == 0) {
-    return(0)
-  }
-
-  # Bereken de afstand van elk relevant overstortpunt tot de meetplaats
-  # st_distance werkt met sf-objecten; het resultaat is een matrix, dus pak de diagonaal of de eerste rij/kolom
-  # aangezien current_location slechts 1 punt is.
-  afstanden <- st_distance(relevant_overstorten, current_location) %>%
-    as.numeric()
-
-  # Voorkom delen door nul als een overstort exact op het meetpunt ligt
-  afstanden[afstanden == 0] <- 0.001
-
-  # Bereken de gewichten met inverse afstand
-  gewichten <- 1 / (afstanden ^ exponent)
-
-  # Bereken de afstandsgewogen cumulatieve BZV
-  weighted_bzv_sum <- sum(relevant_overstorten$BZV * gewichten)
-
-  return(weighted_bzv_sum)
-}
-
-# --- 3. Toepassen op alle meetplaatsen ---
-
-# Initialiseer een nieuwe kolom in het 'locations' sf-object
-locations_cum_gewogen_vuilvracht$weighted_cumulative_BZV <- NA_real_
-
-# Loop over elke meetplaats in het 'locations' sf-object
-for (i in 1:nrow(locations)) {
-  current_meetplaats_id <- locations_cum_gewogen_vuilvracht$meetplaats[i]
-
-  # Bereken de gewogen BZV voor de huidige meetplaats
-  # Gebruik exponent = 1 voor inverse afstand, exponent = 2 voor inverse afstand kwadraat
-  calculated_bzv <- calculate_weighted_bzv(
-    meetplaats_id_val = current_meetplaats_id,
-    overstorten_sf = points_in_watersheds,
-    meetplaatsen_sf = locations_cum_gewogen_vuilvracht,
-    exponent = 1 # Pas dit aan indien je een ander exponent wilt (bv. 2 voor 1/d^2)
-  )
-
-  # Sla het resultaat op
-  locations_cum_gewogen_vuilvracht$weighted_cumulative_BZV[i] <- calculated_bzv
-}
-
-# --- 4. Resultaten bekijken ---
-print(locations_cum_gewogen_vuilvracht)
-
-
-# Merge counts back into watersheds
-cum_vuilvracht_watershed <- watersheds_nested %>%
-  left_join(locations_cum_gewogen_vuilvracht %>%
-              st_drop_geometry(),
-            by = "meetplaats") %>%
-  mutate(cum_gewogen_bzv = replace_na(weighted_cumulative_BZV, 0)) %>%
-  select(-weighted_cumulative_BZV) %>%
-  st_drop_geometry()
-
-# visualisatie
-test <- watersheds_nested %>% filter(meetplaats == "OW389965")
-test2 <- watersheds_buffered %>% filter(meetplaats == "OW389965")
-mapview(test) +
-  mapview(test2) +
-  mapview(vuilvracht_overstorten) +
-  mapview(locations)
-
-save(cum_vuilvracht_watershed, file = here("data", "verwerkt", "overstorten", "mi_meetpunten_vuilvracht_bzv.rdata"))
-
-# Reclassen van landgebruik ----
-
-watershed_landuse <- st_read(here("data", "verwerkt", "landgebruik","finale_watershed_landgebruik.gpkg"))
-
-landuse_reclass <- function(data, suffix) {
-data <- data %>%
-  mutate(!!sym(paste0("water_", suffix)) := VALUE_24_pct,
-         !!sym(paste0("verharding_", suffix)) := VALUE_19_pct + VALUE_22_pct + VALUE_23_pct + VALUE_25_pct,
-         !!sym(paste0("natte_natuur_", suffix)) := VALUE_5_pct + VALUE_9_pct + VALUE_10_pct,
-         !!sym(paste0("landbouw_intens_", suffix)) := VALUE_11_pct + VALUE_18_pct + VALUE_14_pct + VALUE_15_pct,
-         !!sym(paste0("hooggroen_", suffix)) := VALUE_2_pct + VALUE_3_pct + VALUE_4_pct + VALUE_16_pct + VALUE_21_pct,
-         !!sym(paste0("laaggroen_", suffix)) := VALUE_1_pct + VALUE_6_pct + VALUE_7_pct + VALUE_8_pct + VALUE_20_pct,
-         !!sym(paste0("landbouw_extensief_", suffix)) := VALUE_13_pct + VALUE_17_pct
-  ) %>%
-  select(meetplaats, starts_with("water_"), starts_with("verharding_"), starts_with("natte_natuur_"),
-         starts_with("landbouw_intens_"), starts_with("hooggroen_"),
-         starts_with("laaggroen_"), starts_with("landbouw_extensief_")) %>%
-  st_drop_geometry()
-
-return(data)
-}
-
-watershed_landuse_reclass <- landuse_reclass(watershed_landuse, "afstroomgebied") %>%
-  inner_join(., watersheds_buffered %>%
-               select(meetplaats, oppervlakte))
-
-save(watershed_landuse_reclass, file  = here("data", "verwerkt", "landgebruik", "landgebruik_afstroomgebied.Rdata"))
-buffer_landuse_reclass <- landuse_reclass(landuse_buffer, "buffer")
-save(buffer_landuse_reclass, file  = here("data", "verwerkt", "landgebruik", "landgebruik_buffer.Rdata"))
-oever_landuse_reclass <- landuse_reclass(landuse_oever, "oever")
-save(oever_landuse_reclass, file  = here("data", "verwerkt", "landgebruik", "landgebruik_oever.Rdata"))
-
-load(file  = here("data", "verwerkt", "landgebruik", "landgebruik_afstroomgebied.Rdata"))
-
---
+++ source/overstorten.R
@@ -0,0 +1,137 @@
+source(here::here("source", "inladen_packages.R"))
+
+# aantal overstorten in een afstroomgebied ----
+# Dit is op basis van de kaart met overstorten aangeleverd door Ine, ism Karel De Visscher. Basis zijn alle locaties, uitlaat zouden locaties zijn die verbonden zijn met een waterloop.
+overstorten_basis_locaties <- st_read(here("data", "ruw", "overstorten", "P_OS_basis.shp"))
+overstorten_uitlaat_vha <- st_read(here("data", "ruw", "overstorten", "P_OS_uitlaat_VHA.shp"))
+
+watersheds_buffered <- st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_buffered_all.gpkg"))
+
+watersheds_nested <- st_read(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_watersheds_nested_all.gpkg")) #gpkg met alle afstroomgebieden!
+
+# Perform spatial join: Assign points to watersheds
+overstorten_in_afstroomgebied <- st_join(overstorten_uitlaat_vha, watersheds_buffered, left = FALSE)
+
+# Count points per watershed
+watershed_point_counts <- overstorten_in_afstroomgebied  %>%
+  count(meetplaats) %>%
+  rename(aantal_overstorten = n)
+
+# Merge counts back into watersheds
+watersheds_aantal_overstorten <- watersheds_buffered %>%
+  left_join(watershed_point_counts %>%
+              st_drop_geometry(),
+            by = "meetplaats") %>%
+  mutate(aantal_overstorten = replace_na(aantal_overstorten, 0)) %>%
+  st_drop_geometry()
+
+
+# Save the output
+save(watersheds_aantal_overstorten, file = here("data", "verwerkt", "overstorten", "mi_meetpunten_aantal_overstorten_afstroomgebied.rdata"))
+
+# jaarvuilvracht van overstorten in een afstroomgebied met buffer en afstandsgewogen ----
+# Hier bereken ik de cumulatieve vuilvracht (BZV) per afstroomgebied van een meetpunt. Dit wordt afstandsgewogen, omdat een ver overstort minder effect zal hebben dan een dichter (niet kwadratisch; optioneel kan kwadratisch (exponent in functie))
+vuilvracht_overstorten <- st_read(here("data", "ruw" , "overstorten", "vuilvracht", "OS_maart2025.shp"))
+
+locations <- read_sf(here("data", "verwerkt", "hydrologisch", "mi_meetpunten_snapped_to_streams.shp"))
+
+# Perform spatial join: Assign points to watersheds
+points_in_watersheds <- st_join(vuilvracht_overstorten, watersheds_buffered, left = FALSE)
+
+locations_cum_gewogen_vuilvracht <- locations
+
+
+# --- 2. Functie voor afstandsgewogen BZV-berekening ----
+
+# Deze functie berekent de gewogen BZV voor EEN ENKEL meetpunt
+calculate_weighted_bzv <- function(meetplaats_id_val, overstorten_sf, meetplaatsen_sf, exponent = 1) {
+  # Haal de specifieke meetplaats geometrie op
+  current_location <- meetplaatsen_sf %>%
+    filter(meetplaats == meetplaats_id_val)
+
+  # Haal de overstorten op die bij dit meetpunt horen
+  relevant_overstorten <- overstorten_sf %>%
+    filter(meetplaats == meetplaats_id_val)
+
+  # Als er geen relevante overstorten zijn, retourneer 0
+  if (nrow(relevant_overstorten) == 0) {
+    return(0)
+  }
+
+  # Bereken de afstand van elk relevant overstortpunt tot de meetplaats
+  # st_distance werkt met sf-objecten; het resultaat is een matrix, dus pak de diagonaal of de eerste rij/kolom
+  # aangezien current_location slechts 1 punt is.
+  afstanden <- st_distance(relevant_overstorten, current_location) %>%
+    as.numeric()
+
+  # Voorkom delen door nul als een overstort exact op het meetpunt ligt
+  afstanden[afstanden == 0] <- 0.001
+
+  # Bereken de gewichten met inverse afstand
+  gewichten <- 1 / (afstanden ^ exponent)
+
+  # Bereken de afstandsgewogen cumulatieve BZV
+  weighted_bzv_sum <- sum(relevant_overstorten$BZV * gewichten)
+
+  return(weighted_bzv_sum)
+}
+
+# --- 3. Toepassen op alle meetplaatsen ----
+
+# Initialiseer een nieuwe kolom in het 'locations' sf-object
+locations_cum_gewogen_vuilvracht$weighted_cumulative_BZV <- NA_real_
+
+# Loop over elke meetplaats in het 'locations' sf-object
+for (i in 1:nrow(locations)) {
+  current_meetplaats_id <- locations_cum_gewogen_vuilvracht$meetplaats[i]
+
+  # Bereken de gewogen BZV voor de huidige meetplaats
+  # Gebruik exponent = 1 voor inverse afstand, exponent = 2 voor inverse afstand kwadraat
+  calculated_bzv <- calculate_weighted_bzv(
+    meetplaats_id_val = current_meetplaats_id,
+    overstorten_sf = points_in_watersheds,
+    meetplaatsen_sf = locations_cum_gewogen_vuilvracht,
+    exponent = 1 # Pas dit aan indien je een ander exponent wilt (bv. 2 voor 1/d^2)
+  )
+
+  # Sla het resultaat op
+  locations_cum_gewogen_vuilvracht$weighted_cumulative_BZV[i] <- calculated_bzv
+}
+
+# --- 4. Resultaten bekijken ---
+print(locations_cum_gewogen_vuilvracht)
+
+
+# Merge counts back into watersheds
+cum_vuilvracht_watershed <- watersheds_nested %>%
+  left_join(locations_cum_gewogen_vuilvracht %>%
+              st_drop_geometry(),
+            by = "meetplaats") %>%
+  mutate(cum_gewogen_bzv = replace_na(weighted_cumulative_BZV, 0)) %>%
+  select(-weighted_cumulative_BZV) %>%
+  st_drop_geometry()
+
+# visualisatie
+plot <- watersheds_nested %>% filter(meetplaats == "OW100000")
+plot2 <- watersheds_buffered %>% filter(meetplaats == "OW100000")
+plot3 <- points_in_watersheds %>% filter(meetplaats == "OW100000")
+plot4 <- locations %>% filter(meetplaats == "OW100000")
+plot5 <- overstorten_in_afstroomgebied %>% filter(meetplaats == "OW100000")
+mapview(plot) +
+  mapview(plot2) +
+  mapview(plot3) +
+  mapview(plot4) +
+  mapview(plot5)
+
+afstroomgebied_full <- watersheds_nested %>% filter(meetplaats == "OW118000")
+afstroomgebied_buffer <- watersheds_buffered %>% filter(meetplaats == "OW118000")
+overstorten_vuilvracht <- points_in_watersheds %>% filter(meetplaats == "OW118000")
+meetplaats_punt <- locations %>% filter(meetplaats == "OW118000")
+overstorten_uitlaat <- overstorten_in_afstroomgebied %>% filter(meetplaats == "OW118000")
+mapview(afstroomgebied_full) +
+  mapview(afstroomgebied_buffer) +
+  mapview(meetplaats_punt, col.regions = "red") +
+  mapview(overstorten_vuilvracht, col.regions = "green") +
+  mapview(overstorten_uitlaat)
+
+save(cum_vuilvracht_watershed, file = here("data", "verwerkt", "overstorten", "mi_meetpunten_vuilvracht_bzv.rdata"))
